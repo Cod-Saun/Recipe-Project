031567850e1f4923d64a56870d4f1736a4bd6a23
Ubuntu <ubuntu@ip-172-31-28-23.us-west-1.compute.internal>
2019-04-30 22:37:39 +0000

    Login complete

--- Django/recipe/recipeApp/urls.py
+++ Django/recipe/recipeApp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-   path('', views.index),
-]
+        path('', views.Homepage),
+        path('recipes', views.Recipes),
+        path('login', auth_views.LoginView.as_view()),
+        path('logout', views.Logout),
+        path('register', views.Register),
+        path('recommendation', views.Recommendation),
+        ]
--- Django/recipe/recipeApp/views.py
+++ Django/recipe/recipeApp/views.py
@@ -1,8 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from . import forms
+from . import models
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'index.html')
+#for API URL replace q=best with category to search for
+#Replace to=10 with the number of recipes the user would like to search for
+APIKey=""
+
+def Homepage(request):
+  context = {
+      "title":"Homepage",
+      "body":"Hello",
+      "page":"Home"
+      }
+  return render(request, 'index.html', context=context)
+
+def Recipes(request):
+  context = {
+      "title":"Recipes",
+      "page":"Recipes"
+      }
+  return render(request, 'recipes.html', context=context)
+
+@login_required(redirect_field_name='/', login_url="/login")
+def Recommendation(request):
+  context = { 
+      "title":"Recipe Recommendation Search",
+      "page":"Recommendation"
+      }
+  return render(request, 'recommendation.html', context=context)
+
+def Register(request):
+  if request.method == "POST":
+    form_instance = forms.RegistrationForm(request.POST)
+    if form_instance.is_valid():
+      form_instance.save()
+      return redirect("/")
+  else:
+    form_instance = forms.RegistrationForm()
+  context = {
+      "form":form_instance,
+    }
+  return render(request, "registration/register.html", context=context)
+
+def Logout(request):
+  logout(request)
+  return redirect("/login")
